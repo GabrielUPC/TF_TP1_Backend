e25f089cf724494cccd122c00b1f9bccd439c624
GabrielUPC <149513293+GabrielUPC@users.noreply.github.com>
2026-06-06 17:01:46 -0500

    backend 4

--- src/main/java/pe/edu/upc/tf_tp1_backend/Controllers/IpressController.java
+++ src/main/java/pe/edu/upc/tf_tp1_backend/Controllers/IpressController.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/ipress")
+@CrossOrigin(origins = "http://localhost:4200")
 public class IpressController {
 
     @Autowired
--- src/main/java/pe/edu/upc/tf_tp1_backend/Controllers/RolController.java
+++ src/main/java/pe/edu/upc/tf_tp1_backend/Controllers/RolController.java
@@ -11,6 +11,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/roles")
+@CrossOrigin(origins = "http://localhost:4200")
 public class RolController {
 
     @Autowired
